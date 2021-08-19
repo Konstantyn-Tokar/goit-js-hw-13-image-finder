87b37753fded4a728eb62272edc8bb0a98f33ff5
Konstantyn-Tokar <79169268+Konstantyn-Tokar@users.noreply.github.com>
2021-08-19 23:35:07 +0300

    NewsApiServive

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import './sass/main.scss';
+import NewsApiServive from './js/apiService';
 import imageCard from './templates/image_card.hbs';
 
+// ____________________________refs___________________________________
 const refs = {
   searchForm: document.querySelector('#search-form'),
   btnLoadMore: document.querySelector('[data-action="load-more"]'),
 };
 
-console.log(refs.searchForm);
-console.log(refs.btnLoadMore);
+const newsApiServive = new NewsApiServive();
+
+// ____________________________EventListener___________________________________
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.btnLoadMore.addEventListener('click', onLoadMore);
+
+function onSearch(event) {
+  event.preventDefault();
+  newsApiServive.query = event.currentTarget.elements.query.value;
+  newsApiServive.resetPage();
+  newsApiServive.fetchArticles();
+}
+
+function onLoadMore() {
+  newsApiServive.fetchArticles();
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,9 +1,37 @@
 const API_KEY = '22999398-db9a08a82e46fc49ff445f160';
 const BASE_URL = 'https://pixabay.com/api/';
 
-class apiServive {
-  constructor() {}
-  getImages() {
-    const url = `${BASE_URL}?key=${API_KEY}&editors_choice=true${SETTING_IMG}&per_page=1&page=1`;
+export default class NewsApiServive {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    // console.log(this);
+    fetch(
+      `${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        // console.log(this);
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
